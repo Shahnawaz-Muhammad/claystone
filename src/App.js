baa35b1aa958903e12c05765db3c3bbfdbde5b78
Farhan Ajmal <farhanajmal789@gmail.com>
2023-08-10 02:48:54 -0700

    added custom font, colors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='bg-green-400'>hello react</h1>
+      <h1 className='bg-gradient-to-r from-gradientStart to-gradientEnd' >hello react</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      darkBlue: "#11112B",
+      lightBlue: "#D5D9E5",
+      orangePeel: "#FF9D00",
+      gradientStart: "#04C2BE",
+      gradientEnd: "#0AD5D2",
+      white: "#ffffff",
+      blue: "#004D82",
+      linkBlue: "#1876B7",
+    },
+    fontFamily: {
+      "space-grotesk": ["Space Grotesk", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
