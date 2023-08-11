43050bac83ba47b9b8506e3ff41ecba0517c20fd
Shahnawaz-Muhammad <shahnawazbaltee55@gmail.com>
2023-08-11 11:31:35 +0500

    hero heading animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
+import Home from "./pages/home";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Home />
     </>
   );
 }
