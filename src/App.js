da186a89d97bffa7a57e8ff8c193933244dd1362
FahadKhan111 <ak0902586@gmail.com>
2023-08-11 17:56:33 +0500

    our work component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import WhyChooseUs from "./components/whyChooseUs";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
-
+import Ourwork from "./components/our-work"
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <WhyChooseUs />
+
+      <Ourwork />
     </>
   );
 }
