36f78168e1a72b79bad98d3812711e23204c7b22
FahadKhan111 <ak0902586@gmail.com>
2023-08-11 18:10:43 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import WhyChooseUs from "./components/whyChooseUs";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
-import Home from "./pages/home";
 import ClientsStories from "./components/clients-stories";
 import Projects from "./components/projects";
 import AskQuestions from "./components/qna";
