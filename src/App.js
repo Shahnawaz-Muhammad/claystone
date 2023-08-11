1f7bffc72944ace4d570e51203c00f085ca59477
Shahnawaz-Muhammad <shahnawazbaltee55@gmail.com>
2023-08-11 18:02:51 +0500

    clients section, faq section and project section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import WhyChooseUs from "./components/whyChooseUs";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
+import ClientsStories from "./components/clients-stories";
+import Projects from "./components/projects";
+import AskQuestions from "./components/qna";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Hero />
       <WhyChooseUs />
+      <ClientsStories />
+      <Projects />
+      <AskQuestions />
     </>
   );
 }
