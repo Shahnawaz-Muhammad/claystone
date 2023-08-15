23227ede33877cfd70ca777eaaf9c9696a4b2592
Farhan Ajmal <farhanajmal789@gmail.com>
2023-08-15 06:09:40 -0700

    added animation in heading

--- src/components/whyChooseUs/index.jsx
+++ src/components/whyChooseUs/index.jsx
@@ -2,8 +2,41 @@ import ExpertiseAnimation from "../animation/expertiseAnimation";
 import ResultAnimation from "../animation/resultAnimation";
 import SolutionAnimation from "../animation/solutionAnimation";
 import TechnolgyAnimation from "../animation/technologyAnimation";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 const WhyChooseUs = () => {
+  const controls = useAnimation();
+  const headingRef = useRef(null);
+
+  useEffect(() => {
+    const headingElement = headingRef.current;
+
+    const handleScroll = () => {
+      const { top } = headingElement.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+
+      if (top < windowHeight * 0.8) {
+        controls.start("visible");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [controls]);
+
+  const variants = {
+    hidden: { opacity: 0, y: 75 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.6, ease: "easeOut" },
+    },
+  };
+
   const featuresData = [
     {
       img: <ExpertiseAnimation />,
@@ -33,10 +66,18 @@ const WhyChooseUs = () => {
   return (
     <div className="bg-darkBlue text-white pb-[200px]">
       <div className="grid gap-[50px] lg:gap-[80px] w-[90%] lg:w-[95%] xl:w-[86%] mx-auto ">
-        <h3 className="text-[50px] sm:text-[60px] md:text-[80px] lg:text-[100px] text-[#D9D9D9] leading-[60px] sm:leading-[70px] md:leading-[90px] lg:leading-[110px] font-bold ">
-          Why <br />
-          Choose Us?
-        </h3>
+        <motion.div
+          ref={headingRef}
+          initial="hidden"
+          animate={controls}
+          variants={variants}
+          className="text-[50px] sm:text-[60px] md:text-[80px] lg:text-[100px] text-[#D9D9D9] leading-[60px] sm:leading-[70px] md:leading-[90px] lg:leading-[110px] font-bold"
+        >
+          <h3>
+            Why <br />
+            Choose Us?
+          </h3>
+        </motion.div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-[68px]">
           {featuresData.map((data, index) => (
             <div key={index}>
